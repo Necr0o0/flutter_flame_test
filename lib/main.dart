ef3501c99b399e0795a0ae04e093e6631b4c5827
Nero <radekklaser15@gmail.com>
2026-03-28 21:20:35 +0100

    added safe area to game

--- lib/main.dart
+++ lib/main.dart
@@ -29,16 +29,19 @@ class AbsorbApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: Scaffold(
-        body: GameWidget<AbsorbGame>(
-          game: AbsorbGame(),
-          overlayBuilderMap: {
-            'MainMenu': (context, game) => MainMenu(game: game),
-            'GameOver': (context, game) => GameOverScreen(game: game),
-            'HudOverlay': (context, game) => HudOverlay(game: game),
-            'PauseMenu': (context, game) => PauseMenu(game: game)
-          },
-          // Display the MainMenu overlay as soon as the app starts
-          initialActiveOverlays: const ['MainMenu'],
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: GameWidget<AbsorbGame>(
+            game: AbsorbGame(),
+            overlayBuilderMap: {
+              'MainMenu': (context, game) => MainMenu(game: game),
+              'GameOver': (context, game) => GameOverScreen(game: game),
+              'HudOverlay': (context, game) => HudOverlay(game: game),
+              'PauseMenu': (context, game) => PauseMenu(game: game)
+            },
+            // Display the MainMenu overlay as soon as the app starts
+            initialActiveOverlays: const ['MainMenu'],
+          ),
         ),
       ),
     );
